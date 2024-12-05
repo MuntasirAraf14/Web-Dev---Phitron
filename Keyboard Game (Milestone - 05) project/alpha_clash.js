39d494e3a4d06401b3362545f14ed0343b37d00a
MuntasirAraf <112501683+MuntasirAraf@users.noreply.github.com>
2024-12-05 21:16:40 +0600

    Completed game

--- Keyboard Game (Milestone - 05) project/alpha_clash.js
+++ Keyboard Game (Milestone - 05) project/alpha_clash.js
@@ -15,7 +15,14 @@
 function handleKeyboardKeyUpEvent(event){
     const playerPressed = event.key;
 
+
     console.log('Player pressed',playerPressed);
+
+    //stop the game if player pressed escape
+
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
 
     //get the expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -27,16 +34,24 @@ function handleKeyboardKeyUpEvent(event){
     //check matched or not (right or wrong)
     if(playerPressed === expectedAlphabet){
         console.log('you get a point');
+        /*
         //update score
         //get the current score
         const currentScoreElement = document.getElementById('current-score');
         const currentScoreText = currentScoreElement.innerText;
         const currentScore = parseInt(currentScoreText);
         console.log(currentScore);
+
+    
+        */
+
+        const currentScore = getTextElementValueById('current-score');
         //update the score by 1
+        
         const newScore = currentScore + 1;
         //show the updated score
-        currentScoreElement.innerText = newScore;
+        setTextElementValueById('current-score', newScore);
+        //currentScoreElement.innerText = newScore;
         //start a new round
 
 
@@ -46,6 +61,12 @@ function handleKeyboardKeyUpEvent(event){
     }
     else{
         console.log('life lost');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+
+        setTextElementValueById('current-life', updatedLife); 
+        /*
          //get the current life
          const currentLifeElement = document.getElementById('current-life');
          const currentLifeText = currentLifeElement.innerText;
@@ -56,6 +77,11 @@ function handleKeyboardKeyUpEvent(event){
 
          //display the updated life count
          currentLifeElement.innerText  = newLife;
+        */
+
+         if(updatedLife === 0){
+            gameOver();
+         }
     }
 }
 
@@ -77,8 +103,29 @@ function continueGame(){
 }
 
 function play(){
+    //hide everything show only playground
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    //reset score and life
+
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    //update final score
+    //get the final score
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score', lastScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
 }
 
